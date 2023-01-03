5dbaad7b4087fee054dce9effc1e3b03d5e0a304
LO-VE-0 <hsg151515@naver.com>
2023-01-03 14:35:39 +0900

    thread 연습

---
+++ src/thread/generate/ThreadTest1.java
@@ -0,0 +1,24 @@
+package thread.generate;
+
+public class ThreadTest1 extends Thread {
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 100; i++ ) {
+			System.out.println("thread 실행 " + (i+1));
+			
+			try {
+				sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	public static void main(String[] args) {
+		ThreadTest1 t1 = new ThreadTest1(); //new born 상태
+		t1.start(); //running
+	
+
+	}
+
+}
